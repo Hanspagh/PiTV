47815c0344814314bd6d56813ad2fe17cb965f5e
Phil Plückthun <phil@plckthn.me>
2014-12-10 12:12:19 +0100

    Playback Service functions

--- app/scripts/controllers/footer.js
+++ app/scripts/controllers/footer.js
@@ -4,5 +4,26 @@ angular.module('pitvApp')
   .controller('FooterCtrl', function ($scope, PlaybackService) {
 
     $scope.playing = PlaybackService.playing;
+    $scope.label = PlaybackService.label;
+
+    $scope.play = function() {
+      PlaybackService.play();
+    };
+
+    $scope.pause = function() {
+      PlaybackService.pause();
+    };
+
+    $scope.stop = function() {
+      PlaybackService.stop();
+    };
+
+    $scope.forward = function() {
+      PlaybackService.forward();
+    };
+
+    $scope.backward = function() {
+      PlaybackService.backward();
+    };
 
   });
--- app/scripts/services/playbackservice.js
+++ app/scripts/services/playbackservice.js
@@ -4,8 +4,92 @@ angular.module('pitvApp')
   .service('PlaybackService', function ($q, bridge) {
 
     var _playing = false;
+    var _label = "";
+
+    var _play = function() {
+      var defer = $q.defer();
+      bridge.emit('playbackPlay', function(success) {
+        if (success) {
+          _playing = true;
+          defer.resolve();
+        } else {
+          defer.reject();
+        }
+      });
+      return defer.promise;
+    };
+
+    var _pause = function() {
+      var defer = $q.defer();
+      bridge.emit('playbackPause', function(success) {
+        if (success) {
+          _playing = false;
+          defer.resolve();
+        } else {
+          defer.reject();
+        }
+      });
+      return defer.promise;
+    };
+
+    var _stop = function() {
+      var defer = $q.defer();
+      bridge.emit('playbackStop', function(success) {
+        if (success) {
+          _playing = false;
+          defer.resolve();
+        } else {
+          defer.reject();
+        }
+      });
+      return defer.promise;
+   };
+
+    var _start = function(url) {
+      var defer = $q.defer();
+      bridge.emit('playbackStart', url, function(success) {
+        if (success) {
+          _playing = true;
+          defer.resolve();
+        } else {
+          defer.reject();
+        }
+      });
+      return defer.promise;
+    };
+
+    var _forward = function() {
+      var defer = $q.defer();
+      bridge.emit('playbackForward', function(success) {
+        if (success) {
+          defer.resolve();
+        } else {
+          defer.reject();
+        }
+      });
+      return defer.promise;
+    };
+
+    var _backward = function() {
+      var defer = $q.defer();
+      bridge.emit('playbackBackward', function(success) {
+        if (success) {
+          defer.resolve();
+        } else {
+          defer.reject();
+        }
+      });
+      return defer.promise;
+    };
 
     return {
-      playing: _playing
+      playing: _playing,
+      play: _play,
+      pause: _pause,
+      stop: _stop,
+      start: _start,
+      forward: _forward,
+      backward: _backward,
+      label: _label
     };
   });

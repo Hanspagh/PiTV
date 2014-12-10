ddd151c3fc3686775a2f90bbbb09caab235c0424
Phil Plückthun <phil@plckthn.me>
2014-12-10 22:45:31 +0100

    Display movies' ratings

--- app/scripts/controllers/movie.js
+++ app/scripts/controllers/movie.js
@@ -1,7 +1,22 @@
 'use strict';
 
 angular.module('pitvApp')
-  .controller('MovieCtrl', function ($scope, PlaybackService) {
+  .controller('MovieCtrl', function ($scope, $animate, PlaybackService) {
+
+    var container = angular.element($('.stars'));
+    var rating = $scope.movie.rating / 2;
+    var roundRating = Math.round(rating);
+    for(var i = 5; i > 0; i--) {
+      var element;
+      if (i > roundRating) {
+        element = angular.element('<i class="fa fa-star-o reveal"></i>');
+      } else if (i == roundRating && rating % 1 >= .5) {
+        element = angular.element('<i class="fa fa-star-half-o reveal"></i>');
+      } else {
+        element = angular.element('<i class="fa fa-star reveal"></i>');
+      }
+      $animate.enter(element, container);
+    }
 
     $scope.playTorrent = function(index) {
       var url = $scope.torrents[index].magnet;
